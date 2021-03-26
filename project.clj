84284813a9def793272f7f444f5b21511f5d2e65
narocath <pkoromilias@yahoo.com>
2021-03-26 08:24:20 +0200

    added licence url

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject reagent_autocomplete "0.2.1"
   :description "A simple pure ClojureScript autocomplete widget"
   :url "https://github.com/narocath/reagent_autocomplete"
-  :license {:name "MIT"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [reagent "1.0.0"]]
   :source-paths ["src"]
