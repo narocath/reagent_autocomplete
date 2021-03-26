b1980a22ea9688e6d70619914fb4e37c42633091
narocath <pkoromilias@yahoo.com>
2021-03-26 08:04:23 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/narocath/reagent_autocomplete"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [reagent "0.7.0" :exclusions [cljsjs/react]]])
+                 [reagent "1.0.0"]]
+  :source-paths ["src"]
+  :resource-paths ["images"])
