62010c977b05ab2a37580aa6110e533747d3ce38
narocath <pkoromilias@yahoo.com>
2021-03-26 11:39:29 +0200

    fix reagent version from misspelling

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject narocath/reagent_autocomplete "0.2.1"
+(defproject narocath/reagent_autocomplete "0.2.2"
   :description "A simple pure ClojureScript autocomplete widget"
   :url "https://github.com/narocath/reagent_autocomplete"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [reagent "1.0.0"]])
+                 [reagent "0.10.0"]])
