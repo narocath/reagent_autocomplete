c47472558636c6b7727f0adcaadfacd703be3714
narocath <pkoromilias@yahoo.com>
2021-03-26 08:14:22 +0200

    cljstyle

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [reagent "1.0.0"]]
-  :init-ns
   :source-paths ["src"]
   :resource-paths ["images"]
   :uberjar-name "reagent-autocomplete_0.2.1")
