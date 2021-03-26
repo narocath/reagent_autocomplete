432c4e8c37f88b25509acb3e498fb2abfbda9109
narocath <pkoromilias@yahoo.com>
2021-03-26 08:08:57 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject reagent_autocomplete "0.2.0"
+(defproject reagent_autocomplete "0.2.1"
   :description "A simple pure ClojureScript autocomplete widget"
   :url "https://github.com/narocath/reagent_autocomplete"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [reagent "1.0.0"]]
+  :init-ns
   :source-paths ["src"]
-  :resource-paths ["images"])
+  :resource-paths ["images"]
+  :uberjar-name "reagent-autocomplete_0.2.1")
