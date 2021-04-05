3933d769f35eb463bbc6a6c757a6aebeec004157
narocath <pkoromilias@yahoo.com>
2021-04-05 13:53:12 +0300

    updated project.clj version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject narocath/reagent_autocomplete "0.2.3"
+(defproject narocath/reagent_autocomplete "0.2.4"
   :description "A simple pure ClojureScript autocomplete widget"
   :url "https://github.com/narocath/reagent_autocomplete"
   :license {:name "MIT"
