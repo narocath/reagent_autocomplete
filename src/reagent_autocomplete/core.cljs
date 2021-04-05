72240270801082851fad9b6975f5ed726433f6e2
narocath <pkoromilias@yahoo.com>
2021-04-05 13:08:40 +0300

    added ':class' ':id' field on component, also seperate style fields in default-params that would add classes for the styling of the component

--- src/reagent_autocomplete/core.cljs
+++ src/reagent_autocomplete/core.cljs
@@ -21,6 +21,14 @@
    :arrow-right "ArrowRight"
    :tab "Tab"})
 
+(def selected-bg-color
+  {:selected-bg-color "#dcdbfa"
+   :!selected-bg-color "white"})
+
+(def icon
+  {:search-icon search-icon})
+
+
 (def default-params
   {:app-state (reagent/atom {:cur-index 0 :cur-input ""})
 
@@ -28,6 +36,14 @@
    :display-size 10
    :placeholder "Input anything here..."
    :autofocus? true
+
+   :dropdown-classes nil
+   :input-classes nil
+   :background-input-classes nil
+   :click-submit-classes nil
+   :parent-div-classes nil
+   :id nil
+   :style? true
 
    :dropdown-style
    {:position "absolute" :right "0" :left "0"   :top "4em" :box-shadow "grey 1px 2px 1px 0px" :background "white" :overflow "hidden"  :border "none" :z-index "999"}
@@ -42,15 +58,15 @@
    {:position "relative" :box-shadow "1px 1px 1px gray"}
 
    :general-style
-   ".reagent-autocomplete-input:focus{outline:none;}.reagent-autocomplete-item:hover{background:#dcdbfa !important;}"
+   (fn [sl-bg-color] (str ".reagent-autocomplete-input:focus{outline:none;}.reagent-autocomplete-item:hover{background: " sl-bg-color "!important;}"))
 
    :render-click-submit-fn
    (fn [can-submit? on-click]
      (if can-submit?
        [:div
         {:on-click on-click
-         :style {:color "#dcdbfa" :cursor "pointer"}}
-        [:img {:src search-icon}]]))
+         :style {:color (:background-color-selected selected-bg-color) :cursor "pointer"}}
+        [:img {:src (:search-icon icon)}]]))
    :compare-fn
    (fn [cur-input item]
      "Checking whether input matches with the item"
@@ -62,10 +78,11 @@
      "When user clicks enter:"
      (.log js/console "Submitted now: " data))
    :render-fn
-   (fn [cur-input selected? text]
-     [:div.reagent-autocomplete-item {:style {:padding "0.5em" :background (if  selected? "#dcdbfa" "white") :font-size "1em" :cursor "pointer"}}
+   (fn [cur-input selected? text sl-bg-color !sl-bg-color]
+     [:div.reagent-autocomplete-item {:style {:padding "0.5em" :background (if  selected? sl-bg-color !sl-bg-color) :font-size "1em" :cursor "pointer"}}
       [:span cur-input]
       [:b (apply str (drop (count cur-input) text))]])})
+
 
 (defn- e->content
   [e]
@@ -91,12 +108,14 @@
 
 (defn autocomplete-widget
   [completions params]
-  (let [{:keys [app-state
+  (let [{:keys [app-state dropdown-classes input-classes background-input-classes
+                click-submit-classes parent-div-classes id style?
                 can-enter-new? display-size placeholder autofocus?
                 dropdown-style input-style background-input-style parent-div-style
                 click-submit-style general-style
+                selected-bg-color !selected-bg-color
                 compare-fn validate-fn submit-fn render-fn  render-click-submit-fn]}
-        (merge default-params params)
+        (merge icon selected-bg-color default-params params)
         wrap-submit-fn
         (fn [input]
           (if-not (empty? input)
@@ -104,61 +123,121 @@
               (submit-fn input)
               (swap! app-state assoc :cur-input ""))
             identity))]
-    (fn [completions params]
-      (let [aps @app-state
-            cur-input (:cur-input aps)
-            cur-index (:cur-index aps)
-            filtered-items
-            (if-not (empty? cur-input)
-              (for [[index item] (map list (range) (take display-size (filter (partial compare-fn cur-input)  completions)))]
-                {:selected? (= index cur-index)
-                 :name   item})
-              [])
-            complete-placeholder (or (:name (first (filter :selected? filtered-items))) "")
-            has-auto-complete? (not (empty? complete-placeholder))]
-        [:div {:style parent-div-style}
-         [:style general-style]
-         [:div {:style click-submit-style}
-          [render-click-submit-fn
-           (submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete?
-                          #(not (empty? %)))
-           #(submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn)]]
-         [:input.reagent-autocomplete-input
-          {:value cur-input
-           :placeholder placeholder
-           :auto-focus autofocus?
-           :style input-style
-           :type "text"
-           :on-change
-           #(if (validate-fn (e->content %))
-              (change-cur-input app-state (e->content %))
-              identity)
-           :on-key-down
-           #(condp = (aget % "key")
-              (:enter react-keys)
-              (do (submit-policy (e->content %) complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn) true)
-              (:arrow-down react-keys)
-              (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items)))
-              (:arrow-up react-keys)
-              (swap! app-state assoc :cur-index (mod (dec cur-index) (count filtered-items)))
-              (:arrow-right react-keys)
-              (change-cur-input app-state complete-placeholder)
-              (:tab react-keys)
-              (do
-                (cond
-                  (= 1 (count filtered-items))
-                  (change-cur-input app-state complete-placeholder)
-                  :else
-                  (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items))))
-                (.call (aget % "preventDefault") %))
-              identity)}]
-         [:input.reagent-autocomplete-input {:style  background-input-style :disabled true
-                                             :placeholder complete-placeholder}]
-         (if (not (empty? filtered-items))
-           [:div {:style dropdown-style}
-            (for [item filtered-items]
-              ^{:key (:name item)}
-              [:div {:on-click #(wrap-submit-fn (:name item))}
-               [render-fn cur-input (:selected? item) (:name item)]])]
-           [:div])]))))
-
+    (cond
+      style? (fn [completions params]
+               (let [aps @app-state
+                     cur-input (:cur-input aps)
+                     cur-index (:cur-index aps)
+                     filtered-items
+                     (if-not (empty? cur-input)
+                       (for [[index item] (map list (range) (take display-size (filter (partial compare-fn cur-input)  completions)))]
+                         {:selected? (= index cur-index)
+                          :name   item})
+                       [])
+                     complete-placeholder (or (:name (first (filter :selected? filtered-items))) "")
+                     has-auto-complete? (seq complete-placeholder)]
+                 (.log js/console "style")
+                 [:div {:style parent-div-style}
+                  [:style (general-style selected-bg-color)]
+                  [:div {:style click-submit-style}
+                   [render-click-submit-fn
+                    (submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete?
+                                   #(not (empty? %)))
+                    #(submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn)]]
+                  [:input.reagent-autocomplete-input
+                   {:value cur-input
+                    :placeholder placeholder
+                    :auto-focus autofocus?
+                    :style input-style
+                    :type "text"
+                    :on-change
+                    #(if (validate-fn (e->content %))
+                       (change-cur-input app-state (e->content %))
+                       identity)
+                    :on-key-down
+                    #(condp = (aget % "key")
+                       (:enter react-keys)
+                       (do (submit-policy (e->content %) complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn) true)
+                       (:arrow-down react-keys)
+                       (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items)))
+                       (:arrow-up react-keys)
+                       (swap! app-state assoc :cur-index (mod (dec cur-index) (count filtered-items)))
+                       (:arrow-right react-keys)
+                       (change-cur-input app-state complete-placeholder)
+                       (:tab react-keys)
+                       (do
+                         (cond
+                           (= 1 (count filtered-items))
+                           (change-cur-input app-state complete-placeholder)
+                           :else
+                           (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items))))
+                         (.call (aget % "preventDefault") %))
+                       identity)}]
+                  [:input.reagent-autocomplete-input {:style  background-input-style :disabled true
+                                                      :placeholder complete-placeholder}]
+                  (if (not (empty? filtered-items))
+                    [:div {:style dropdown-style}
+                     (for [item filtered-items]
+                       ^{:key (:name item)}
+                       [:div {:on-click #(wrap-submit-fn (:name item))}
+                        [render-fn cur-input (:selected? item) (:name item) selected-bg-color !selected-bg-color]])]
+                    [:div])]))
+      :else (fn [completions params]
+              (let [aps @app-state
+                    cur-input (:cur-input aps)
+                    cur-index (:cur-index aps)
+                    filtered-items
+                    (if-not (empty? cur-input)
+                      (for [[index item] (map list (range) (take display-size (filter (partial compare-fn cur-input)  completions)))]
+                        {:selected? (= index cur-index)
+                         :name   item})
+                      [])
+                    complete-placeholder (or (:name (first (filter :selected? filtered-items))) "")
+                    has-auto-complete? (not (empty? complete-placeholder))]
+                [:div {:class parent-div-classes
+                       :id id}
+                 [:style (general-style selected-bg-color)]
+                 [:div {:class click-submit-classes}
+                  [render-click-submit-fn
+                   (submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete?
+                                  #(not (empty? %)))
+                   #(submit-policy cur-input complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn)]]
+                 [:input.reagent-autocomplete-input
+                  {:value cur-input
+                   :placeholder placeholder
+                   :auto-focus autofocus?
+                   :class input-classes
+                   :type "text"
+                   :on-change
+                   #(if (validate-fn (e->content %))
+                      (change-cur-input app-state (e->content %))
+                      identity)
+                   :on-key-down
+                   #(condp = (aget % "key")
+                      (:enter react-keys)
+                      (do (submit-policy (e->content %) complete-placeholder can-enter-new? has-auto-complete? wrap-submit-fn) true)
+                      (:arrow-down react-keys)
+                      (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items)))
+                      (:arrow-up react-keys)
+                      (swap! app-state assoc :cur-index (mod (dec cur-index) (count filtered-items)))
+                      (:arrow-right react-keys)
+                      (change-cur-input app-state complete-placeholder)
+                      (:tab react-keys)
+                      (do
+                        (cond
+                          (= 1 (count filtered-items))
+                          (change-cur-input app-state complete-placeholder)
+                          :else
+                          (swap! app-state assoc :cur-index (mod (inc cur-index) (count filtered-items))))
+                        (.call (aget % "preventDefault") %))
+                      identity)}]
+                 [:input.reagent-autocomplete-input {:class background-input-classes :disabled true
+                                                     :style {:z-index "-1"}
+                                                     :placeholder complete-placeholder}]
+                 (if (seq filtered-items)
+                   [:div {:class dropdown-classes}
+                    (for [item filtered-items]
+                      ^{:key (:name item)}
+                      [:div {:on-click #(wrap-submit-fn (:name item))}
+                       [render-fn cur-input (:selected? item) (:name item) selected-bg-color !selected-bg-color]])]
+                   [:div])])))))
